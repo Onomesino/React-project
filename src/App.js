7b8ef54334f819fbb329de0adf87e141b2114c9c
Onomesino Esther <onomesinoesther@Onomesinos-MacBook-Pro.local>
2022-06-21 15:49:14 +0000

    react

--- src/App.js
+++ src/App.js
@@ -3,22 +3,42 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="container fff">
+		<div class="row">
+			<div class="col-md-12 second">0</div>
+		</div>
+		 <div class="row">
+			<div  class="col-md-3 fi">AC</div>
+			<div  class="col-md-3 fi">+/-</div>
+			<div  class="col-md-3 fi">%</div>
+			<div  class="col-md-3 box">÷</div>
+		</div>
+
+		 <div class="row">
+			<div  class="col-md-3 fi">7</div>
+			<div  class="col-md-3 fi">8</div>
+			<div  class="col-md-3 fi">9</div>
+			<div  class="col-md-3 box">x</div>
+		</div>
+		 <div class="row">
+			<div  class="col-md-3 fi">4</div>
+			<div  class="col-md-3 fi">5</div>
+			<div  class="col-md-3 fi">6</div>
+			<div  class="col-md-3 box">-</div>
+		</div>
+
+		 <div class="row">
+			<div  class="col-md-3 fi">1</div>
+			<div  class="col-md-3 fi">2</div>
+			<div  class="col-md-3 fi">3</div>
+			<div  class="col-md-3 box">+ </div>
+		</div>
+		 <div class="row">
+			<div  class="col-md-6 fi">0</div>
+			<div  class="col-md-3 fi">.</div>
+			<div  class="col-md-3 box">=</div>
+		</div>
+	</div>
   );
 }
 
